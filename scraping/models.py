a92c81450170f0cae5bbb1f2e154a71613b70492
Stanislav <nstanislass@gmail.com>
2023-02-08 14:10:46 +0200

    add model City

--- scraping/models.py
+++ scraping/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+
+class City(models.Model):
+    name = models.CharField('Название населенного пункта', max_length=100, unique=True)
+    slug = models.SlugField('url', max_length=100, unique=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    class Meta:
+        verbose_name = 'Название населенного пункта'
+        verbose_name_plural = 'Название населенных пунктов'
