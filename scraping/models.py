b5af0d07a0fa2efcc0da66b1a3c9743f2a49f672
Stanislav <nstanislass@gmail.com>
2023-02-08 14:11:22 +0200

    add model Language

--- scraping/models.py
+++ scraping/models.py
@@ -17,3 +17,20 @@ class City(models.Model):
     class Meta:
         verbose_name = 'Название населенного пункта'
         verbose_name_plural = 'Название населенных пунктов'
+
+
+class Language(models.Model):
+    name = models.CharField('Язык программирования', max_length=100, unique=True)
+    slug = models.SlugField('url', max_length=100, unique=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    class Meta:
+        verbose_name = 'Язык программирования'
+        verbose_name_plural = 'Языки программирования'
