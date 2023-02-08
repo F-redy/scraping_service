70ef4483f198c9d7c2e904988a9ba047d95b3713
Stanislav <nstanislass@gmail.com>
2023-02-08 14:14:45 +0200

    add forms.py add form FindForm

---
+++ scraping/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from .models import *
+
+
+class FindForm(forms.Form):
+    city = forms.ModelChoiceField(
+        queryset=City.objects.all(),
+        empty_label='Город не выбран',
+        to_field_name='slug',
+        required=False,
+        widget=forms.Select(attrs={'class': 'form-control'}),
+        label='Город'
+    )  # required - делает не обязательным поля для заполнения
+
+    language = forms.ModelChoiceField(
+        queryset=Language.objects.all(),
+        empty_label='Специальность не выбрана',
+        to_field_name='slug',
+        required=False,
+        widget=forms.Select(attrs={'class': 'form-control'}),
+        label='Специальность'
+    )
