4e8244ffd082621d524d3d9b3c9b2643990d222c
Stanislav <nstanislass@gmail.com>
2023-02-16 15:52:20 +0200

    add function register_view

--- accounts/views.py
+++ accounts/views.py
@@ -23,3 +23,13 @@ def logout_view(request):
     return redirect('home')
 
 
+def register_view(request):
+    form = UserRegistrationForm(request.POST or None)
+
+    if form.is_valid():
+        new_user = form.save(commit=False)
+        new_user.set_password(form.cleaned_data['password'])
+        new_user.save()
+        return render(request, 'accounts/register_done.html', {'new_user': new_user})
+
+    return render(request, 'accounts/register.html', {'form': form})
