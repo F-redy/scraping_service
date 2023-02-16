c7804411dd704cdefd1ee5a6ebadd241233e2f3b
Stanislav <nstanislass@gmail.com>
2023-02-16 15:50:44 +0200

    add function login_view

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+
+from accounts.forms import UserLoginForm, UserRegistrationForm
 
 
-def register(request):
-    return render(request, 'accounts/register.html')
+def login_view(request):
+    form = UserLoginForm(request.POST or None)
+    if form.is_valid():
+        data = form.cleaned_data
+        email = data.get('email')
+        password = data.get('password')
+        user = authenticate(request, email=email, password=password)
+        login(request, user)
+
+        return redirect('home')
+
+    return render(request, 'accounts/login.html', {'form': form})
+
+
